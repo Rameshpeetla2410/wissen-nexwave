84cd7bd05d87fcfd9284e9688aeef1867b4f8a91
nagabhushanam <nagabhushanamn@gmail.com>
2018-06-20 14:48:10 +0530

    IO

---
+++ 18-java-IO/src/com/model/Location.java
@@ -0,0 +1,16 @@
+package com.model;
+
+public class Location {
+	private String name;
+
+	public Location(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Location [name=" + name + "]";
+	}
+
+}
